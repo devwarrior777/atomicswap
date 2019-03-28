64d96fb18c232347f06ab6c304da6e9468283402
devwarrior <devwarrior.sec@gmail.com>
2019-03-28 18:55:04 +0800

    refactor updated server/binding

--- libs/protobind/wallets.go
+++ libs/protobind/wallets.go
@@ -1,4 +1,4 @@
-package libs
+package protobind
 
 //
 // For Golang client - ignore this file as we do not need a server.
@@ -12,31 +12,31 @@ import (
 	"fmt"
 
 	"github.com/devwarrior777/atomicswap/libs/ltc"
-	bnd "github.com/devwarrior777/atomicswap/libs/protobind"
 	"github.com/devwarrior777/atomicswap/libs/xzc"
+	//...
 )
 
 // PingWalletRPC checks if the wallet node is available for the coin and network
 // The server wants an error returned but we embed our errors in the binding. If
 // the response gets to the client with an error it means there was a transport error
-func PingWalletRPC(request *bnd.PingWalletRPCRequest) (*bnd.PingWalletRPCResponse, error) {
+func PingWalletRPC(request *PingWalletRPCRequest) (*PingWalletRPCResponse, error) {
 	switch request.Coin {
 	//case pb.COIN_BTC:
 	//	return newAddressBtc(testnet, hostport, rpcuser, rpcpass)
-	case bnd.COIN_LTC:
+	case COIN_LTC:
 		return pingWalletRPCLtc(request), nil
-	case bnd.COIN_XZC:
+	case COIN_XZC:
 		return pingWalletRPCXzc(request), nil
 		//
 		//...more coins
 	}
-	response := &bnd.PingWalletRPCResponse{Errorno: bnd.ERRNO_UNSUPPORTED}
+	response := &PingWalletRPCResponse{Errorno: ERRNO_UNSUPPORTED}
 	response.Errstr = fmt.Sprintf("unsupported coin: %v", request.Coin)
 	return response, nil
 }
 
-func pingWalletRPCLtc(request *bnd.PingWalletRPCRequest) *bnd.PingWalletRPCResponse {
-	response := &bnd.PingWalletRPCResponse{}
+func pingWalletRPCLtc(request *PingWalletRPCRequest) *PingWalletRPCResponse {
+	response := &PingWalletRPCResponse{}
 	rpcinfo := ltc.RPCInfo{
 		HostPort: request.Hostport,
 		User:     request.Rpcuser,
@@ -44,17 +44,17 @@ func pingWalletRPCLtc(request *bnd.PingWalletRPCRequest) *bnd.PingWalletRPCRespo
 	}
 	err := ltc.PingRPC(request.Testnet, rpcinfo)
 	if err != nil {
-		response.Errorno = bnd.ERRNO_LIBS
+		response.Errorno = ERRNO_LIBS
 		response.Errstr = err.Error()
 		return response
 	}
 
-	response.Errorno = bnd.ERRNO_OK
+	response.Errorno = ERRNO_OK
 	return response
 }
 
-func pingWalletRPCXzc(request *bnd.PingWalletRPCRequest) *bnd.PingWalletRPCResponse {
-	response := &bnd.PingWalletRPCResponse{}
+func pingWalletRPCXzc(request *PingWalletRPCRequest) *PingWalletRPCResponse {
+	response := &PingWalletRPCResponse{}
 	rpcinfo := xzc.RPCInfo{
 		HostPort: request.Hostport,
 		User:     request.Rpcuser,
@@ -62,12 +62,12 @@ func pingWalletRPCXzc(request *bnd.PingWalletRPCRequest) *bnd.PingWalletRPCRespo
 	}
 	err := xzc.PingRPC(request.Testnet, rpcinfo)
 	if err != nil {
-		response.Errorno = bnd.ERRNO_LIBS
+		response.Errorno = ERRNO_LIBS
 		response.Errstr = err.Error()
 		return response
 	}
 
-	response.Errorno = bnd.ERRNO_OK
+	response.Errorno = ERRNO_OK
 	return response
 }
 
